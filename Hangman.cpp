8a8df131e86a85d9593019821e3c4c218a2f58c2
Stuti19 <63838650+Stuti19@users.noreply.github.com>
2023-10-27 17:22:49 +0530

    Update Hangman.cpp

--- Hangman.cpp
+++ Hangman.cpp
@@ -3,7 +3,6 @@
 //Players can take turns or work together. 
 //Gameplay continues until the players guess the word or they run out of guesses and the stick figure is hung.
 
-
 #include <iostream>
 #include <string>
 #include <cstdlib>
@@ -29,66 +28,66 @@ void HangMan(int wrongGuess)
     //No of wrong guesses=5
     if(wrongGuess==5)
     {
-        cout<<"     _______\n";
-        cout<<   "|/      |\n";
-        cout<<   "|      (_)\n";
-        cout<<  "|     __|__\n";
-        cout<<  "|       |\n";
-        cout<<   "|     __|__\n";
-        cout<<  "|\n";
-        cout<<  "|___\n";
+        std::cout<<"     _______\n";
+        std::cout<<   "|/      |\n";
+        std::cout<<   "|      (_)\n";
+        std::cout<<  "|     __|__\n";
+        std::cout<<  "|       |\n";
+        std::cout<<   "|     __|__\n";
+        std::cout<<  "|\n";
+        std::cout<<  "|___\n";
     }
     
     //No of wrong guesses=4
     if(wrongGuess==4)
     {
-        cout<<"     _______\n";
-        cout<<   "|/      |\n";
-        cout<<   "|      (_)\n";
-        cout<<  "|     __|__\n";
-        cout<<  "|       |\n";
-        cout<<   "|       |\n";
-        cout<<  "|\n";
-        cout<<  "|___\n";
+        std::cout<<"     _______\n";
+        std::cout<<   "|/      |\n";
+        std::cout<<   "|      (_)\n";
+        std::cout<<  "|     __|__\n";
+        std::cout<<  "|       |\n";
+        std::cout<<   "|       |\n";
+        std::cout<<  "|\n";
+        std::cout<<  "|___\n";
     }
 
     //No of wrong guesses=3
     if(wrongGuess==3)
     {
-        cout<<"     _______\n";
-        cout<<   "|/      |\n";
-        cout<<   "|      (_)\n";
-        cout<<  "|       |\n";
-        cout<<  "|       |\n";
-        cout<<   "|       |\n";
-        cout<<  "|\n";
-        cout<<  "|___\n";
+        std::cout<<"     _______\n";
+        std::cout<<   "|/      |\n";
+        std::cout<<   "|      (_)\n";
+        std::cout<<  "|       |\n";
+        std::cout<<  "|       |\n";
+        std::cout<<   "|       |\n";
+        std::cout<<  "|\n";
+        std::cout<<  "|___\n";
     }
 
     //No of wrong guesses=2
     if(wrongGuess==2)
     {
-        cout<<"     _______\n";
-        cout<<   "|/      |\n";
-        cout<<   "|      (_)\n";
-        cout<<  "|\n";
-        cout<<  "|\n";
-        cout<<   "|\n";
-        cout<<  "|\n";
-        cout<<  "|___\n";
+        std::cout<<"     _______\n";
+        std::cout<<   "|/      |\n";
+        std::cout<<   "|      (_)\n";
+        std::cout<<  "|\n";
+        std::cout<<  "|\n";
+        std::cout<<   "|\n";
+        std::cout<<  "|\n";
+        std::cout<<  "|___\n";
     }
 
     //No of wrong guess=1
     if(wrongGuess==1)
     {
-        cout<<"     _______\n";
-        cout<<   "|/      |\n";
-        cout<<   "|\n";
-        cout<<  "|\n";
-        cout<<  "|\n";
-        cout<<   "|\n";
-        cout<<  "|\n";
-        cout<<  "|___\n";
+        std::cout<<"     _______\n";
+        std::cout<<   "|/      |\n";
+        std::cout<<   "|\n";
+        std::cout<<  "|\n";
+        std::cout<<  "|\n";
+        std::cout<<   "|\n";
+        std::cout<<  "|\n";
+        std::cout<<  "|___\n";
     }
 }
 
@@ -173,55 +172,55 @@ int main ()
 
 	//Welcome the user
 
-    cout << "\n\n...WELCOME...\n\n";
+    std::cout << "\n\n...WELCOME...\n\n";
 
-    cout << " _   _                                         " << endl;   
-    cout << "| | | |                                        " << endl;
-    cout << "| |_| |  __ _ _ __   __ _ _ __ ___   __ _ _ __ " << endl;
-    cout << "|  _  | / _` | '_ | / _` | '_ ` _ | / _` | '_ |" << endl;
-    cout << "| | | | (_| | | | | (_| | | | | | | (_| | | | |" << endl;
-    cout << "|_| |_| __,_|_| |_| __, |_| |_| |_| __,_|_| |_|" << endl;
-    cout << "                    __/ |                      " << endl;
-    cout << "                   |___/                       " << endl;
+    std::cout << " _   _                                         " << endl;   
+    std::cout << "| | | |                                        " << endl;
+    std::cout << "| |_| |  __ _ _ __   __ _ _ __ ___   __ _ _ __ " << endl;
+    std::cout << "|  _  | / _` | '_ | / _` | '_ ` _ | / _` | '_ |" << endl;
+    std::cout << "| | | | (_| | | | | (_| | | | | | | (_| | | | |" << endl;
+    std::cout << "|_| |_| __,_|_| |_| __, |_| |_| |_| __,_|_| |_|" << endl;
+    std::cout << "                    __/ |                      " << endl;
+    std::cout << "                   |___/                       " << endl;
 
 
-	cout << endl;
-    cout << "Guess name of a state of India";
-    cout << endl;
-    cout << endl;
-	cout << "Each letter is represented by a star.";
-    cout << endl;
-	cout << "You have to type only one letter in one try";
-    cout << endl;
-	cout << "You have " << MAX_TRIES << " tries to try and guess the word.";
-    cout << endl;
-	cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+	std::cout << endl;
+    std::cout << "Guess name of a state of India";
+    std::cout << endl;
+    std::cout << endl;
+	std::cout << "Each letter is represented by a star.";
+    std::cout << endl;
+	std::cout << "You have to type only one letter in one try";
+    std::cout << endl;
+	std::cout << "You have " << MAX_TRIES << " tries to try and guess the word.";
+    std::cout << endl;
+	std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
 
     //Rules
-    cout << endl;
-    cout << endl;
-    cout << "RULES:";
-    cout << endl;
-    cout << "If a guessed letter is present in the word, it is revealed in the correct positions.";
-    cout << endl;
-	cout << "If a guessed letter is not in the word, a part of the hangman figure is drawn.";
-    cout << endl;
-    cout << "The game continues until the players guess the word correctly or the hangman figure is completed.";
-    cout << endl;
-    cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+    std::cout << endl;
+    std::cout << endl;
+    std::cout << "RULES:";
+    std::cout << endl;
+    std::cout << "If a guessed letter is present in the word, it is revealed in the correct positions.";
+    std::cout << endl;
+	std::cout << "If a guessed letter is not in the word, a part of the hangman figure is drawn.";
+    std::cout << endl;
+    std::cout << "The game continues until the players guess the word correctly or the hangman figure is completed.";
+    std::cout << endl;
+    std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
 
     //Continue the loop until the available guesses are exhausted
 	while (wrongGuess < MAX_TRIES)
 	{
-	    cout << endl;
-	    cout << endl;
-        cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
-        cout << endl;
-        cout << "Word is :  ";
-		cout << unknown;
-        cout << endl;
-        cout << endl;
-		cout << "Guess a letter: ";
+	    std::cout << endl;
+	    std::cout << endl;
+        std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+        std::cout << endl;
+        std::cout << "Word is :  ";
+		std::cout << unknown;
+        std::cout << endl;
+        std::cout << endl;
+		std::cout << "Guess a letter: ";
 		cin >> letter;
 
 		//Fill secret word with letter if the guess is correct,
@@ -230,7 +229,7 @@ int main ()
         //if wrong letter was guessed
 		if (letterCheck(letter, word, unknown)==0)
 		{
-			cout << endl << "Umm! Sorry,but the guess was incorrect" << endl;
+			std::cout << endl << "Umm! Sorry,but the guess was incorrect" << endl;
 			wrongGuess++;
 			HangMan(wrongGuess);
 		}
@@ -238,19 +237,19 @@ int main ()
         //if correct letter was guessed
 		else
 		{
-			cout << endl << "Woah!! You found a letter" << endl;
+			std::cout << endl << "Woah!! You found a letter" << endl;
 		}
 
 		//Number of guesses left
-        cout << endl;
-		cout << "You have " << MAX_TRIES - wrongGuess;
-		cout << " guesses left." << endl;
+        std::cout << endl;
+		std::cout << "You have " << MAX_TRIES - wrongGuess;
+		std::cout << " guesses left." << endl;
 
 		//Guessed correct word
 		if (word==unknown)
 		{
-			cout << word << endl;
-			cout << "Congratulations! You got it!";
+			std::cout << word << endl;
+			std::cout << "Congratulations! You got it!";
 			break;
 		}
 	}
@@ -260,13 +259,13 @@ int main ()
 
 	if(wrongGuess == MAX_TRIES)
 	{
-        cout << endl;
-		cout << "Sorry, you have run out of tries....";
-        cout << endl;
-        cout << "You've been hanged";
-        cout << endl;
-		cout << "The word was : " << word;
-        cout << endl;
+        std::cout << endl;
+		std::cout << "Sorry, you have run out of tries....";
+        std::cout << endl;
+        std::cout << "You've been hanged";
+        std::cout << endl;
+		std::cout << "The word was : " << word;
+        std::cout << endl;
 	}
 
 
